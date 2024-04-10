0dbc07cda498acc41710f50f00a4c8c35a7d0e1f
Yuanzaixiong <55530775+Ynkcc@users.noreply.github.com>
2024-04-10 21:46:17 +0800

    binaryToJson&jsonToBinary

--- attic/world/build.sh
+++ attic/world/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# -static -O3 aarch64-linux-gnu-g++ arm-linux-gnueabi-g++
 c++ -std=c++11 -I../.. -I../../ext -I.. -g -o mkworld ../../node/C25519.cpp ../../node/Salsa20.cpp ../../node/SHA512.cpp ../../node/Identity.cpp ../../node/Utils.cpp ../../node/InetAddress.cpp ../../osdep/OSUtils.cpp mkworld.cpp -lm
--- attic/world/mkworld.cpp
+++ attic/world/mkworld.cpp
@@ -16,21 +16,21 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * This utility makes the World from the configuration specified below.
- * It probably won't be much use to anyone outside ZeroTier, Inc. except
- * for testing and experimentation purposes.
- *
- * If you want to make your own World you must edit this file.
- *
- * When run, it expects two files in the current directory:
- *
- * previous.c25519 - key pair to sign this world (key from previous world)
- * current.c25519 - key pair whose public key should be embedded in this world
- *
- * If these files do not exist, they are both created with the same key pair
- * and a self-signed initial World is born.
- */
+ /*
+  * This utility makes the World from the configuration specified below.
+  * It probably won't be much use to anyone outside ZeroTier, Inc. except
+  * for testing and experimentation purposes.
+  *
+  * If you want to make your own World you must edit this file.
+  *
+  * When run, it expects two files in the current directory:
+  *
+  * previous.c25519 - key pair to sign this world (key from previous world)
+  * current.c25519 - key pair whose public key should be embedded in this world
+  *
+  * If these files do not exist, they are both created with the same key pair
+  * and a self-signed initial World is born.
+  */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -49,31 +49,39 @@
 #include <osdep/OSUtils.hpp>
 
 using namespace ZeroTier;
+using json = nlohmann::json;
 
-int main(int argc,char **argv)
-{
-	std::string previous,current;
-	if ((!OSUtils::readFile("previous.c25519",previous))||(!OSUtils::readFile("current.c25519",current))) {
+void printHelp() {
+	printf("Usage: mkworld [options]\n");
+	printf("Options:\n");
+	printf("  -h, --help          Display this help message\n");
+	printf("  -j, --json2bin      Convert from JSON file to world.bin\n");
+	printf("  -b, --bin2json      Convert from world.bin to JSON format\n");
+}
+
+int jsonToBinary() {
+	std::string previous, current;
+	if ((!OSUtils::readFile("previous.c25519", previous)) || (!OSUtils::readFile("current.c25519", current))) {
 		C25519::Pair np(C25519::generate());
 		previous = std::string();
-		previous.append((const char *)np.pub.data,ZT_C25519_PUBLIC_KEY_LEN);
-		previous.append((const char *)np.priv.data,ZT_C25519_PRIVATE_KEY_LEN);
+		previous.append((const char*)np.pub.data, ZT_C25519_PUBLIC_KEY_LEN);
+		previous.append((const char*)np.priv.data, ZT_C25519_PRIVATE_KEY_LEN);
 		current = previous;
-		OSUtils::writeFile("previous.c25519",previous);
-		OSUtils::writeFile("current.c25519",current);
-		fprintf(stderr,"INFO: created initial world keys: previous.c25519 and current.c25519 (both initially the same)" ZT_EOL_S);
+		OSUtils::writeFile("previous.c25519", previous);
+		OSUtils::writeFile("current.c25519", current);
+		fprintf(stderr, "INFO: created initial world keys: previous.c25519 and current.c25519 (both initially the same)" ZT_EOL_S);
 	}
 
-	if ((previous.length() != (ZT_C25519_PUBLIC_KEY_LEN + ZT_C25519_PRIVATE_KEY_LEN))||(current.length() != (ZT_C25519_PUBLIC_KEY_LEN + ZT_C25519_PRIVATE_KEY_LEN))) {
-		fprintf(stderr,"FATAL: previous.c25519 or current.c25519 empty or invalid" ZT_EOL_S);
+	if ((previous.length() != (ZT_C25519_PUBLIC_KEY_LEN + ZT_C25519_PRIVATE_KEY_LEN)) || (current.length() != (ZT_C25519_PUBLIC_KEY_LEN + ZT_C25519_PRIVATE_KEY_LEN))) {
+		fprintf(stderr, "FATAL: previous.c25519 or current.c25519 empty or invalid" ZT_EOL_S);
 		return 1;
 	}
 	C25519::Pair previousKP;
-	memcpy(previousKP.pub.data,previous.data(),ZT_C25519_PUBLIC_KEY_LEN);
-	memcpy(previousKP.priv.data,previous.data() + ZT_C25519_PUBLIC_KEY_LEN,ZT_C25519_PRIVATE_KEY_LEN);
+	memcpy(previousKP.pub.data, previous.data(), ZT_C25519_PUBLIC_KEY_LEN);
+	memcpy(previousKP.priv.data, previous.data() + ZT_C25519_PUBLIC_KEY_LEN, ZT_C25519_PRIVATE_KEY_LEN);
 	C25519::Pair currentKP;
-	memcpy(currentKP.pub.data,current.data(),ZT_C25519_PUBLIC_KEY_LEN);
-	memcpy(currentKP.priv.data,current.data() + ZT_C25519_PUBLIC_KEY_LEN,ZT_C25519_PRIVATE_KEY_LEN);
+	memcpy(currentKP.pub.data, current.data(), ZT_C25519_PUBLIC_KEY_LEN);
+	memcpy(currentKP.priv.data, current.data() + ZT_C25519_PUBLIC_KEY_LEN, ZT_C25519_PRIVATE_KEY_LEN);
 
 	// =========================================================================
 	// EDIT BELOW HERE
@@ -83,117 +91,142 @@ int main(int argc,char **argv)
 	const uint64_t id = ZT_WORLD_ID_EARTH;
 	const uint64_t ts = 1567191349589ULL; // August 30th, 2019
 
-
-
-
-	using json = nlohmann::json;
-
-		std::string fileContent;
-		if (!OSUtils::readFile("config.json", fileContent)) {
-			fprintf(stderr, "Failed to open config file." ZT_EOL_S);
-			return 1;
-		}
-
-		// 解析JSON数据
-		json config = json::parse(fileContent);
-
-
-		for (auto& root : config["roots"]) {
-			roots.push_back(World::Root());
-			roots.back().identity = Identity(root["identity"].get<std::string>().c_str());
-			for (auto& endpoint : root["stableEndpoints"]) {
-				std::string ip = endpoint["ip"];
-				int port = endpoint["port"];
-				std::string address = ip + "/" + std::to_string(port);
-				roots.back().stableEndpoints.push_back(InetAddress(address.c_str()));
-			}
-		}
-
-
-
-
-	// Los Angeles
-
-
-	//// Miami
-	//roots.push_back(World::Root());
-	//roots.back().identity = Identity("de8950a8b2:0:1b3ada8251b91b6b6fa6535b8c7e2460918f4f729abdec97d3c7f3796868fb02f0de0b0ee554b2d59fc3524743eebfcf5315e790ed6d92db5bd10c28c09b40ef");
-	//roots.back().stableEndpoints.push_back(InetAddress("207.246.73.245/443"));
-	//roots.back().stableEndpoints.push_back(InetAddress("2001:19f0:9002:5cb:ec4:7aff:fe8f:69d9/443"));
-
-	//// Tokyo
-	//roots.push_back(World::Root());
-	//roots.back().identity = Identity("34e0a5e174:0:93efb50934788f856d5cfb9ca5be88e85b40965586b75befac900df77352c145a1ba7007569d37c77bfe52c0999f3bdc67a47a4a6000b720a883ce47aa2fb7f8");
-	//roots.back().stableEndpoints.push_back(InetAddress("147.75.92.2/443"));
-	//roots.back().stableEndpoints.push_back(InetAddress("2604:1380:3000:7100::1/443"));
-
-	//// Amsterdam
-	//roots.push_back(World::Root());
-	//roots.back().identity = Identity("992fcf1db7:0:206ed59350b31916f749a1f85dffb3a8787dcbf83b8c6e9448d4e3ea0e3369301be716c3609344a9d1533850fb4460c50af43322bcfc8e13d3301a1f1003ceb6");
-	//roots.back().stableEndpoints.push_back(InetAddress("195.181.173.159/443"));
-	//roots.back().stableEndpoints.push_back(InetAddress("2a02:6ea0:c024::/443"));
-
-	// Alice
-	//roots.push_back(World::Root());
-	//roots.back().identity = Identity("9d219039f3:0:01f0922a98e3b34ebcbff333269dc265d7a020aab69d72be4d4acc9c8c9294785771256cd1d942a90d1bd1d2dca3ea84ef7d85afe6611fb43ff0b74126d90a6e");
-	//roots.back().stableEndpoints.push_back(InetAddress("188.166.94.177/9993")); // Amsterdam
-	//roots.back().stableEndpoints.push_back(InetAddress("2a03:b0c0:2:d0::7d:1/9993")); // Amsterdam
-	//roots.back().stableEndpoints.push_back(InetAddress("154.66.197.33/9993")); // Johannesburg
-	//roots.back().stableEndpoints.push_back(InetAddress("2c0f:f850:154:197::33/9993")); // Johannesburg
-	//roots.back().stableEndpoints.push_back(InetAddress("159.203.97.171/9993")); // New York
-	//roots.back().stableEndpoints.push_back(InetAddress("2604:a880:800:a1::54:6001/9993")); // New York
-	//roots.back().stableEndpoints.push_back(InetAddress("131.255.6.16/9993")); // Buenos Aires
-	//roots.back().stableEndpoints.push_back(InetAddress("2803:eb80:0:e::2/9993")); // Buenos Aires
-	//roots.back().stableEndpoints.push_back(InetAddress("107.170.197.14/9993")); // San Francisco
-	//roots.back().stableEndpoints.push_back(InetAddress("2604:a880:1:20::200:e001/9993")); // San Francisco
-	//roots.back().stableEndpoints.push_back(InetAddress("128.199.197.217/9993")); // Singapore
-	//roots.back().stableEndpoints.push_back(InetAddress("2400:6180:0:d0::b7:4001/9993")); // Singapore
-
-	// Bob
-	//roots.push_back(World::Root());
-	//roots.back().identity = Identity("8841408a2e:0:bb1d31f2c323e264e9e64172c1a74f77899555ed10751cd56e86405cde118d02dffe555d462ccf6a85b5631c12350c8d5dc409ba10b9025d0f445cf449d92b1c");
-	//roots.back().stableEndpoints.push_back(InetAddress("45.32.198.130/9993")); // Dallas
-	//roots.back().stableEndpoints.push_back(InetAddress("2001:19f0:6400:81c3:5400:00ff:fe18:1d61/9993")); // Dallas
-	//roots.back().stableEndpoints.push_back(InetAddress("46.101.160.249/9993")); // Frankfurt
-	//roots.back().stableEndpoints.push_back(InetAddress("2a03:b0c0:3:d0::6a:3001/9993")); // Frankfurt
-	//roots.back().stableEndpoints.push_back(InetAddress("107.191.46.210/9993")); // Paris
-	//roots.back().stableEndpoints.push_back(InetAddress("2001:19f0:6800:83a4::64/9993")); // Paris
-	//roots.back().stableEndpoints.push_back(InetAddress("45.32.246.179/9993")); // Sydney
-	//roots.back().stableEndpoints.push_back(InetAddress("2001:19f0:5800:8bf8:5400:ff:fe15:b39a/9993")); // Sydney
-	//roots.back().stableEndpoints.push_back(InetAddress("45.32.248.87/9993")); // Tokyo
-	//roots.back().stableEndpoints.push_back(InetAddress("2001:19f0:7000:9bc9:5400:00ff:fe15:c4f5/9993")); // Tokyo
-	//roots.back().stableEndpoints.push_back(InetAddress("159.203.2.154/9993")); // Toronto
-	//roots.back().stableEndpoints.push_back(InetAddress("2604:a880:cad:d0::26:7001/9993")); // Toronto
-
-	// END WORLD DEFINITION
-	// =========================================================================
-
-	fprintf(stderr,"INFO: generating and signing id==%llu ts==%llu" ZT_EOL_S,(unsigned long long)id,(unsigned long long)ts);
-
-	World nw = World::make(World::TYPE_PLANET,id,ts,currentKP.pub,roots,previousKP);
-
-	Buffer<ZT_WORLD_MAX_SERIALIZED_LENGTH> outtmp;
-	nw.serialize(outtmp,false);
-	World testw;
-	testw.deserialize(outtmp,0);
-	if (testw != nw) {
-		fprintf(stderr,"FATAL: serialization test failed!" ZT_EOL_S);
+	std::string fileContent;
+	if (!OSUtils::readFile("config.json", fileContent)) {
+		fprintf(stderr, "Failed to open config file." ZT_EOL_S);
 		return 1;
 	}
 
-	OSUtils::writeFile("world.bin",std::string((const char *)outtmp.data(),outtmp.size()));
-	fprintf(stderr,"INFO: world.bin written with %u bytes of binary world data." ZT_EOL_S,outtmp.size());
+	// 解析JSON数据
+	json config = json::parse(fileContent);
 
-	fprintf(stdout,ZT_EOL_S);
-	fprintf(stdout,"#define ZT_DEFAULT_WORLD_LENGTH %u" ZT_EOL_S,outtmp.size());
-	fprintf(stdout,"static const unsigned char ZT_DEFAULT_WORLD[ZT_DEFAULT_WORLD_LENGTH] = {");
-	for(unsigned int i=0;i<outtmp.size();++i) {
-		const unsigned char *d = (const unsigned char *)outtmp.data();
-		if (i > 0)
-			fprintf(stdout,",");
-		fprintf(stdout,"0x%.2x",(unsigned int)d[i]);
+
+	for (auto& root : config["roots"]) {
+		roots.push_back(World::Root());
+		roots.back().identity = Identity(root["identity"].get<std::string>().c_str());
+		for (auto& endpoint : root["stableEndpoints"]) {
+			roots.back().stableEndpoints.push_back(InetAddress(endpoint.get<std::string>().c_str()));
+		}
 	}
-	fprintf(stdout,"};" ZT_EOL_S);
+
+	fprintf(stderr, "INFO: generating and signing id==%llu ts==%llu" ZT_EOL_S, (unsigned long long)id, (unsigned long long)ts);
+
+	World nw = World::make(World::TYPE_PLANET, id, ts, currentKP.pub, roots, previousKP);
+
+	Buffer<ZT_WORLD_MAX_SERIALIZED_LENGTH> outtmp;
+	nw.serialize(outtmp, false);
+	World testw;
+	testw.deserialize(outtmp, 0);
+	if (testw != nw) {
+		fprintf(stderr, "FATAL: serialization test failed!" ZT_EOL_S);
+		return 1;
+	}
+
+	OSUtils::writeFile("world.bin", std::string((const char*)outtmp.data(), outtmp.size()));
+	fprintf(stderr, "INFO: world.bin written with %u bytes of binary world data." ZT_EOL_S, outtmp.size());
+
+	fprintf(stdout, ZT_EOL_S);
+	fprintf(stdout, "#define ZT_DEFAULT_WORLD_LENGTH %u" ZT_EOL_S, outtmp.size());
+	fprintf(stdout, "static const unsigned char ZT_DEFAULT_WORLD[ZT_DEFAULT_WORLD_LENGTH] = {");
+	for (unsigned int i = 0; i < outtmp.size(); ++i) {
+		const unsigned char* d = (const unsigned char*)outtmp.data();
+		if (i > 0)
+			fprintf(stdout, ",");
+		fprintf(stdout, "0x%.2x", (unsigned int)d[i]);
+	}
+	fprintf(stdout, "};" ZT_EOL_S);
+	return 0;
+}
+
+void binaryToJson() {
+	// Read world.bin file into memory
+	std::string binContent;
+	if (!OSUtils::readFile("world.bin", binContent)) {
+		fprintf(stderr, "Failed to open world.bin file." ZT_EOL_S);
+		return;
+	}
+
+	// Deserialize the binary data into a World object
+	Buffer<ZT_WORLD_MAX_SERIALIZED_LENGTH> binBuffer(binContent.data(), binContent.size());
+	World world;
+	if (!world.deserialize(binBuffer, 0)) {
+		fprintf(stderr, "Failed to deserialize world.bin content." ZT_EOL_S);
+		return;
+	}
+
+	// Create a JSON object to store the world data
+	json worldJson;
+
+
+	// Add roots array to the JSON object
+	json rootsJson;
+	for (const auto& root : world.roots()) {
+		json rootJson;
+
+		// Add identity to the root JSON object
+		char identityStr[ZT_IDENTITY_STRING_BUFFER_LENGTH];
+		root.identity.toString(true, identityStr); // Include private key
+		rootJson["identity"] = identityStr;
+
+		// Add stableEndpoints array to the root JSON object
+		json stableEndpointsJson;
+		for (const auto& endpoint : root.stableEndpoints) {
+			char ipStr[64];
+			endpoint.toString(ipStr);
+			stableEndpointsJson.push_back(ipStr);
+		}
+		rootJson["stableEndpoints"] = stableEndpointsJson;
+
+		rootsJson.push_back(rootJson);
+	}
+	worldJson["roots"] = rootsJson;
+	std::string jsonStr = worldJson.dump(4);
+	printf("World JSON:\n%s\n", jsonStr.c_str());
+	if (!OSUtils::writeFile("config.json", jsonStr.c_str(), jsonStr.size())) {
+		fprintf(stderr, "Failed to write JSON data to config.json." ZT_EOL_S);
+	}
+	else {
+		printf("JSON data successfully written to config.json." ZT_EOL_S);
+	}
+}
+
+
+int main(int argc, char** argv)
+{
+	bool json2bin = false;
+	bool bin2json = false;
+
+	for (int i = 1; i < argc; ++i) {
+		std::string arg = argv[i];
+		if (arg == "-h" || arg == "--help") {
+			printHelp();
+			return 0;
+		}
+		else if (arg == "-j" || arg == "--json2bin") {
+			json2bin = true;
+		}
+		else if (arg == "-b" || arg == "--bin2json") {
+			bin2json = true;
+		}
+	}
+
+	if (!(json2bin || bin2json)) {
+		// Default behavior: convert from JSON to world.bin
+		json2bin = true;
+	}
+
+	if (json2bin && bin2json) {
+		printf("Error: Cannot specify both JSON to binary and binary to JSON conversion options.\n");
+		printHelp();
+		return 1;
+	}
+
+	if (json2bin) {
+		jsonToBinary();
+	}
+	else if (bin2json) {
+		binaryToJson();
+	}
 
 	return 0;
 }
